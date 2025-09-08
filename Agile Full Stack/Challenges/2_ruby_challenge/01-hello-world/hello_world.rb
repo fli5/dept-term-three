2c8f3e3691af0d267992778cce4db85d8b1a24d1
Felix Li <fli5@academic.rrc.ca>
2025-09-08 18:09:10 -0500

    Add U2 - OO Ruby Challenges Assignment for Ruby

---
+++ Agile Full Stack/Challenges/2_ruby_challenge/01-hello-world/hello_world.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# ---------------------------------------------------------------------
+# Copyright (c) 2025. Felix Li. All rights reserved
+# Unauthorized copying, modification, or distribution of this file, via any
+# medium, is strictly prohibited without prior written permission from Felix Li.
+# For licensing inquiries, please contact: fli5@academic.rrc.ca
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+# Program: Full Stack Web Development
+# Name: Feng Li
+# Course: WEBD-3011 (273794) Agile Full Stack Web Development
+# Created: 2025-09-08
+# ---------------------------------------------------------------------
